22400fd0ff89f19fc9e80cc6b9797720f161e64b
taehun <cocoseasky@naver.com>
2024-02-16 17:43:36 +0900

    오늘의 수업 끝

---
+++ 2.web-api/App/updown/assets/js/app.js
@@ -0,0 +1,12 @@
+
+
+// index.html에는 app.js(현재파일) 이 import 되어 있습니다.
+// 현재 파일을 실행부로 취급해서 각각의 기능은 파일별로 따로 구현해 보겠습니다.
+
+import gameStart from "./start.js";
+// import {x, name} from "./start.js";
+
+(function() {
+  gameStart();
+ // console.log(`import로 전달받은 값: ${x}, ${name}`);
+})();
